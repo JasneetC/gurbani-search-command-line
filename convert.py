59b8401529a277976c8b3a9f105f7d5ae6f40159
Karandeep Chawla <ikarandeep@gmail.com>
2014-05-24 03:12:29 -0700

    using gurbani akhar to unicode converter to display unicode

---
+++ convert.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+def convert(letter):
+	gurmukhi_english = ["a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z"," ","1","2","@","3","4","5","6","^","7","&","8","9","0","[","]"]
+	gurmukhi_unicode = [u"ੳ",u"ਆ",u"ਬ","ਭ",u"ਚ",u"ਛ",u"ਦ",u"ਧ",u"ੲ",u"ਓ",u"ਡ",u"ਢ",u"ਗ",u"ਘ",u"ਹ",u"੍ਹ",u"ਿ",u"ੀ",u"ਜ",u"ਝ",u"ਕ",u"ਖ",u"ਲ",u"ਲ਼",u"ਮ",u"ੰ",u"ਨ",u"ਂਂ",u"ੋ",u"ੌ",u"ਪ",u"ਫ",u"ਤ",u"ਥ",u"ਰ",u"੍",u"ਸ",u"ਸ਼",u"ਟ",u"ਠ",u"ੁ",u"ੂ",u"ਵ",u"ੜ",u"ਾ",u"ਾਂ",u"ਣ",u"ਯ",u"ੇ",u"ੈ",u"ਜ਼",u"ਗ਼",u" ",u"੧",u"੨",u"ਹ੍ਹ",u"੩",u"੪",u"੫",u"੬",u"ਖ਼",u"੭",u"ਫ਼",u"੮",u"੯",u"੦",u"।",u"॥"]
+	return gurmukhi_unicode[gurmukhi_english.index(letter)]
+
+
+
+def convertLine(shabadLine):
+	shabad_unicode = []
+	# if the tukh has "ir", we should swap the characters or else the output is incorrect
+	for i in range(len(shabadLine)):
+		index_to_convert = i
+		shabad_unicode.append(convert(shabadLine[index_to_convert]))
+		if shabad_unicode[i-1] == u"ਿ" and shabad_unicode[i] == u"ਰ":
+			temp = shabad_unicode[i]
+			shabad_unicode[i] = shabad_unicode[i-1]
+			shabad_unicode[i-1] = temp
+
+	return "".join(shabad_unicode)
--- gurbani-command-line.py
+++ gurbani-command-line.py
@@ -2,6 +2,7 @@
 #http://igurbani.com/iGurbaniService.php?&mode=1&q=gmsshn&src=0&type=0&writer=0&raag=0&ang=&recnum=0&format=json
 #http://igurbani.com/iGurbaniService.php?&mode=2&shabadNo=1528&format=json
 import json, requests
+import convert
 
 def searchFirstLetters(firstLetters):
 	url = "http://igurbani.com/iGurbaniService.php"
@@ -9,30 +10,43 @@ def searchFirstLetters(firstLetters):
 	resp = requests.get(url=url, params=params)
 	data = resp.json()
 	for shabad in data['shabads']:
-		print [shabad][0]['shabad']['ShabadId'] + " : " + [shabad][0]['shabad']['Transliteration']
+		print [shabad][0]['shabad']['ShabadNo'] + " : " + [shabad][0]['shabad']['Transliteration']
 
 def searchShabadWithID(shabadID):
 	url = "http://igurbani.com/iGurbaniService.php"
-	params = dict(mode=1,q=shabadID,src=0,type=0,writer=0,raag=0,ang='',recnum=0,format="json")
+	params = dict(mode=2,shabadNo=shabadID,format="json")
 	resp = requests.get(url=url, params=params)
 	data = resp.json()
-	for shabad in data['shabads']:
-		print [shabad][0]['shabad']['ShabadId'] + " : " + [shabad][0]['shabad']['Transliteration']
+	for shabad in data['gurbani']:
+		# convert the line
+		gurmukhi = convert.convertLine([shabad][0]['shabad']['Gurmukhi'])
+		print gurmukhi
+		print " "
+		print [shabad][0]['shabad']['Transliteration']
+		print " "
+		print [shabad][0]['shabad']['English']
+		print "----------------------------------------"
 
 def main():
 	inputItem = "continue"
 	while inputItem != "quit":
-		print "-------------------------------------------"
-		print "---- iGurbani Command Line Interface   ----"
-		print "---- Developed by Karandeep Singh      ----"
-		print "----                                   ----"
-		print "Enter a ShabadID or search by first letters"
-		print "-------------------------------------------"
+		print "----------------------------------------------"
+		print "----    Gurbani Command Line Interface    ----"
+		print "----    Use iGurbani.com to search        ----"
+		print "----    for shabads from your terminal!   ----"
+		print "----                                      ----"
+		print "----    Search by first letters           ----"
+		print "----    Enter 'quit' to exit              ----"
+		print "----------------------------------------------"
 		inputItem = raw_input()
-		if isinstance(inputItem,int):
+		if inputItem == "quit":
+			print "exiting"
+			break
+		elif inputItem.isdigit():
 			searchShabadWithID(inputItem)
-		if isinstance(inputItem,str):
+		elif isinstance(inputItem,str):
 			searchFirstLetters(inputItem)
+
 
 
 
